3503ec719a0d2c420e2f8563afc9a06da655fe55
PancaGamaStore <108522948+PancaGamaStore@users.noreply.github.com>
2025-04-11 21:42:47 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
+const express = require("express");
+const axios = require('axios');
+const fs = require('fs');
+const path = require("path");
+
+const app = express();
+const router = express.Router();
+const PORT = process.env.PORT || 3000;
+
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server berjalan di http://localhost:${PORT}`);
 });
